0c7656662654485387a42601df18f0bd11758af8
Charlie Prince <charlescprince@gmail.com>
2026-09-17 12:58:59 -0500

    Colour the UI navy and amber, set in IBM Plex Sans and Barlow Condensed (#90)
    * Colour the UI navy and amber, set in IBM Plex Sans and Barlow Condensed
    
    * Say what self-hosting the UI fonts buys, without claiming offline use

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,5 +1,9 @@
 import { browserStore } from './browserStore';
 import { mountApp } from './mountApp';
+import '@fontsource/barlow-condensed/600.css';
+import '@fontsource/barlow-condensed/700.css';
+import '@fontsource/ibm-plex-sans/400.css';
+import '@fontsource/ibm-plex-sans/600.css';
 import './global.css';
 
 mountApp(document.getElementById('root')!, browserStore);
